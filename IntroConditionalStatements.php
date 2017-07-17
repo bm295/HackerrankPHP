97bc224182cd27b993a14bbd5be726a5d0601fb7
bm295 <baominh.nguyen.295@gmail.com>
2017-07-18 00:24:25 +0800

    Create IntroConditionalStatements.php

---
+++ IntroConditionalStatements.php
@@ -0,0 +1,17 @@
+<?php
+
+$handle = fopen ("php://stdin","r");
+fscanf($handle,"%d",$N);
+if ($N % 2 == 1) {
+    echo "Weird";
+}
+else if ($N >= 2 && $N <= 5) {
+    echo "Not Weird";
+}
+else if ($N >= 6 && $N <= 20) {
+    echo "Weird";
+}
+else if ($N > 20) {
+    echo "Not Weird";
+}
+?>
