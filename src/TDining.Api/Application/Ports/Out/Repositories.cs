411fb9e600118044d295a84b478369483a75d3e5
bm295 <baominh.nguyen.295@gmail.com>
2026-06-21 19:55:35 +0700

    Add xUnit tests for order use cases

---
+++ src/TDining.Api/Application/Ports/Out/Repositories.cs
@@ -0,0 +1,45 @@
+using TDining.Api.Domain.Entities;
+
+namespace TDining.Api.Application.Ports.Out;
+
+public interface IOrderRepository
+{
+    Task AddAsync(Order order, CancellationToken cancellationToken);
+    Task<Order?> GetByIdAsync(Guid orderId, CancellationToken cancellationToken);
+    Task<IReadOnlyCollection<Order>> ListAsync(CancellationToken cancellationToken);
+}
+
+public interface ITableRepository
+{
+    Task<DiningTable?> GetByCodeAsync(string tableCode, CancellationToken cancellationToken);
+    Task<IReadOnlyCollection<DiningTable>> ListAsync(CancellationToken cancellationToken);
+}
+
+public interface IMenuRepository
+{
+    Task<IReadOnlyCollection<MenuItem>> ListAsync(CancellationToken cancellationToken);
+    Task<MenuItem?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+}
+
+public interface IInventoryRepository
+{
+    Task<InventoryItem?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<IReadOnlyCollection<InventoryItem>> ListAsync(CancellationToken cancellationToken);
+}
+
+public interface IPaymentRepository
+{
+    Task AddAsync(Payment payment, CancellationToken cancellationToken);
+    Task<IReadOnlyCollection<Payment>> ListByOrderAsync(Guid orderId, CancellationToken cancellationToken);
+}
+
+public interface IReservationRepository
+{
+    Task AddAsync(Reservation reservation, CancellationToken cancellationToken);
+    Task<IReadOnlyCollection<Reservation>> ListAsync(CancellationToken cancellationToken);
+}
+
+public interface IUnitOfWork
+{
+    Task SaveChangesAsync(CancellationToken cancellationToken);
+}
